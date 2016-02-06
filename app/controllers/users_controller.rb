7459c58b39a5276b32368c30cea68b6af8493771
Michal Kimle <kimle.michal@gmail.com>
2016-02-07 00:27:09 +0100

    fixed query issue for postgres

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,8 @@ class UsersController < ApplicationController
     user_id = params[:id]
     @user = User.confirmed.includes(:skills).includes(:evaluations).find(user_id)
     @projects = Project.includes(:karmas)
-    .references(:karmas)
     .includes(:participants)
     .references(:positions)
     .where('positions.user_id = ? or projects.author_id = ?', user_id, user_id)
-    .group('projects.id')
-    .order('count(karmas.id)')
   end
 end

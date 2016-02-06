290628fe1e4a2f3660d2c5800174b4ed078232e4
Michal Kimle <kimle.michal@gmail.com>
2016-02-07 00:27:34 +0100

    fixed counting of user projects

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   scope :confirmed, -> { where('confirmed_at IS NOT NULL') }
 
   def project_count
-    Project.where('author_id = ?', id).count + Position.includes(:project).references(:project).where('user_id = ? and projects.author_id != ?', id, id).count
+    Project.where('author_id = ?', id).count + Position.select(:project_id).distinct.includes(:project).references(:project).where('user_id = ? and projects.author_id != ?', id, id).count
   end
 
   def sent_messages
